6aaf7e35bfb528f5493ce98c285f8114f316f2ed
Liam Segura <102547056+liamsegura@users.noreply.github.com>
2023-07-22 18:39:41 +0100

    added animation

--- frontend/src/components/HelpPopup.js
+++ frontend/src/components/HelpPopup.js
@@ -1,6 +1,6 @@
 // HelpPopup.js
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import GuidedShoppingForm from './GuidedShoppingForm';
 
 
@@ -18,6 +18,7 @@ const HelpPopup = ({}) => {
     setIsModalOpen(false);
   };
 
+
   return (
     <>
    
@@ -25,7 +26,7 @@ const HelpPopup = ({}) => {
       {/* Modal */}
       {isModalOpen ? (
         <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-black bg-opacity-50">
-          <div className="bg-white p-6 rounded-lg shadow-lg mx-4">
+          <div className="bg-white p-6 rounded-lg shadow-lg mx-4 ">
             <GuidedShoppingForm />
 
             <button
@@ -37,11 +38,11 @@ const HelpPopup = ({}) => {
           </div>
         </div>
 
-      ): ( <div className="fixed bottom-4 right-4 p-8 bg-indigo-500 rounded-lg shadow-lg text-white">
+      ): ( <div className=" fixed text-center bottom-4 right-4 p-8 bg-indigo-500 rounded-lg shadow-lg text-white animate-popIn ">
       <p className='text-xl'>Need some help?</p>
       <button
         onClick={openModal}
-        className="mt-8 px-4 py-2 bg-white text-indigo-500 font-bold rounded-lg hover:bg-gray focus:outline-none focus:ring focus:ring-gray focus:ring-opacity-50"
+        className="mt-8 px-4 py-2 bg-white text-indigo-500 font-bold rounded-lg hover:bg-gray focus:outline-none focus:ring focus:ring-gray focus:ring-opacity-50 animate-growShrink"
       >
         Find out more
       </button>
