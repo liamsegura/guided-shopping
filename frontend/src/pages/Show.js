d012b7bfcd073652658eb5fdaa326c93f24f1417
Liam Segura <102547056+liamsegura@users.noreply.github.com>
2023-07-23 10:18:30 +0100

    changed form, added show

--- frontend/src/pages/Show.js
+++ frontend/src/pages/Show.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { URL } from "../router/url";
+import URL from "../router/url"
 
 
 
--- frontend/src/router/url.js
+++ frontend/src/router/url.js
@@ -1,7 +1,7 @@
-// eslint-disable-next-line
+// // eslint-disable-next-line
 export default "https://odd-jade-crow-slip.cyclic.app/"
 
 
-//localhost
+// //localhost
 // eslint-disable-next-line
 // export default "http://localhost:4444";
