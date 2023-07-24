0a338fb1ced195772f8dc4b3ec4f8fa417b012fa
Liam Segura <102547056+liamsegura@users.noreply.github.com>
2023-07-24 12:25:47 +0100

    react-ga

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { RouterProvider } from "react-router-dom";
 import router from "./router/router";
+import ReactGA from "react-ga";
+
+const TRACKING_ID = "G-3Q65LMK3C5";
+ReactGA.initialize(TRACKING_ID);
 
 // Ensure the document is available in a browser environment
 if (typeof document !== "undefined") {
