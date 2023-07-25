e33f2dfdd891d378ed5b70395ea0acf3cf22afa5
Liam Segura <102547056+liamsegura@users.noreply.github.com>
2023-07-25 18:37:12 +0100

    update

--- frontend/src/router/url.js
+++ frontend/src/router/url.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line
-// export default "https://odd-jade-crow-slip.cyclic.app";
+export default "https://odd-jade-crow-slip.cyclic.app";
 // localhost
 // eslint-disable-next-line
-export default "http://localhost:4444";
+// export default "http://localhost:4444";
